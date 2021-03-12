df3f0e9824102bea0b9807612448fdb8929eca41
Crocodeev <svin862003@mail.ru>
2021-03-12 21:57:58 +0300

    tect c

--- app/renderer/components/Download.js
+++ app/renderer/components/Download.js
@@ -5,13 +5,9 @@ export default function Download(props)  {
   //реализовать should companent update для функционального компонента
 
     return (
-      <div className="row teal lighten-5 valign-wrapper downloadrow">
-       <div className="col s3 center-align">
-       <span className="flow-text teal-text text-darken-1">
-        <h6>download progress:</h6>
-       </span>
-       </div>
-      <div className="col s9">
+      <div className="input-field row teal lighten-5 valign-wrapper downloadrow">
+      <label>DOWNLOAD PROGRESS</label>  
+      <div className="col s12">
        <div className="progress download">
         <div className="determinate" style={
           {width: (props.downloadCount * 100/props.totalTracks ) +'%'}
